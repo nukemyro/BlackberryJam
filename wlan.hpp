a49cbebe20ae9313c2011bbfc9c1d49b7c665232
Anthony Kalsatos <akalsatos@gmail.com>
2017-07-05 15:35:56 -0400

    style-issues: fixed up spacing and added a newline at the end of every file.

--- wlan.hpp
+++ wlan.hpp
@@ -9,9 +9,9 @@ typedef int Outcome;
 #define NOK -1
 
 typedef int FrameType;
-#define MANAGEMENT_FRAME  0x00;
-#define CONTROL_FRAME	  0x01
-#define DATA_FRAME        0x02
+#define MANAGEMENT_FRAME 0x00;
+#define CONTROL_FRAME 0x01
+#define DATA_FRAME 0x02
 
 typedef int MANAGEMENT_SUB_TYPE;
 #define PROBE_REQUEST 0x04;
@@ -29,46 +29,39 @@ typedef int DATA_SUB_TYPE;
 #define CF_POLL_NODATA 0x06;
 #define CF_ACK_POLL_NODATA 0x07;
 
-
 #define MAX_NAME_LEN 128
 
 // Declaration of struct WLANAddr.
 #define WLAN_ADDR_LEN 6
-struct WLANAddr
-{
-   // address
-   unsigned char data[WLAN_ADDR_LEN];
-   // return the address in a human readable form
-   char * wlan2asc();
-   // defined the address from a human readable form
-   Outcome str2wlan(char *);
+struct WLANAddr {
+    // address
+    unsigned char data[WLAN_ADDR_LEN];
+    // return the address in a human readable form
+    char* wlan2asc();
+    // defined the address from a human readable form
+    Outcome str2wlan(char*);
 
-   bool operator==(const WLANAddr & addr)
-   {
-	   if 	(addr.data[0] == data[0] &&
-			 addr.data[1] == data[1] &&
-			 addr.data[2] == data[2] &&
-			 addr.data[3] == data[3] &&
-			 addr.data[4] == data[4] &&
-			 addr.data[5] == data[5]) return true;
-	   else return false;
-   }
+    bool operator==(const WLANAddr& addr)
+    {
+        if (addr.data[0] == data[0] && addr.data[1] == data[1] && addr.data[2] == data[2] && addr.data[3] == data[3] && addr.data[4] == data[4] && addr.data[5] == data[5])
+            return true;
+        else
+            return false;
+    }
 };
 
 // format of an WLAN header
-struct WLANHeader
-{
-   __u64 destAddr;
-   __u64 srcAddr;
+struct WLANHeader {
+    __u64 destAddr;
+    __u64 srcAddr;
 };
 #define WLAN_HEADER_LEN 12 // bytes
 
 // wireless interface configuration
-struct Ifconfig
-{
-   int sockid; // socket descriptor
-   int ifindex; // index of the interface
-   WLANAddr hwaddr; // MAC address
-   int mtu; // maximum transmission unit
+struct Ifconfig {
+    int sockid; // socket descriptor
+    int ifindex; // index of the interface
+    WLANAddr hwaddr; // MAC address
+    int mtu; // maximum transmission unit
 };
 
